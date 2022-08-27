6e4f55f3c094e74b2ba2baefb360ff3c8d074152
Bin Yang <yangbin880101@163.com>
2022-08-27 15:58:53 -0400

    fixed error in curve2D.curve1Dexpression

---
+++ build/lib/polykriging/mesh.py
@@ -0,0 +1,383 @@
+from polykriging import utility
+import numpy as np
+from shapely.geometry import Polygon, Point
+import pyvista as pv
+from pyvista import _vtk
+
+
+def isInbBox(bbox, point):
+    """
+    Determine if point is within a bounding box (bbox)
+    that parallel to the principle axes of global Cartesian coordinate.
+
+    :param bbox: bounding box, list: [xmin, xmax, ymin, ymax, zmin, zmax]
+    :param point: point, list: [x, y, z]
+    :return: True or False
+    """
+    x, y, z = point[0], point[1], point[2]
+    xmin, ymin, zmin = bbox[0], bbox[1], bbox[2]
+    xmax, ymax, zmax = bbox[3], bbox[4], bbox[5]
+    boolst = [True, True, True, False, False, False]
+    boo = [x > xmin, y > ymin, z > zmin, x > xmax, y > ymax, z > zmax]
+    return boo == boolst
+
+
+def background_mesh_generator(bbox, voxel_size=None):
+    """
+    Generate a voxel background mesh.
+    :param bbox: bounding box of the background mesh specified through a numpy array
+        contains the minimum and maximum coordinates of the bounding box
+        [xmin, xmax, ymin, ymax, zmin, zmax]
+    :param voxel_size: voxel size of the background mesh, type: None, float, or numpy.ndarray
+        if None, the voxel size is set to the 1/20 of the diagonal length of the bounding box;
+        if float, the voxel size is set to the float value in x, y, z directions;
+        if numpy.ndarray, the voxel size is set to the values in the numpy.ndarray for corresponding directions.
+    :return: pyvista mesh object (UnstructuredGrid)
+    """
+    # get the size of the bounding box
+    size = np.array([bbox[1] - bbox[0], bbox[3] - bbox[2], bbox[5] - bbox[4]])
+    # get the diagonal length of the bounding box
+    diagonal = np.linalg.norm(size)
+
+    if voxel_size is None:
+        # Return (1/20) length of the diagonal of the bounding box.
+        voxel_size_x, voxel_size_y, voxel_size_z = diagonal / 20, diagonal / 20, diagonal / 20
+    if isinstance(voxel_size, (int, float)):
+        voxel_size_x, voxel_size_y, voxel_size_z = voxel_size, voxel_size, voxel_size
+    if isinstance(voxel_size, (list, set, tuple)):
+        voxel_size_x, voxel_size_y, voxel_size_z = voxel_size
+
+    xrng = np.linspace(bbox[0], bbox[1], int((bbox[1] - bbox[0]) / voxel_size_x) + 1)
+    yrng = np.linspace(bbox[2], bbox[3], int((bbox[3] - bbox[2]) / voxel_size_y) + 1)
+    zrng = np.linspace(bbox[4], bbox[5], int((bbox[5] - bbox[4]) / voxel_size_z) + 1)
+
+    x, y, z = np.meshgrid(xrng, yrng, zrng)
+    grid = pv.StructuredGrid(x, y, z)
+    mesh_shape = np.array(x.shape) - 1
+    print(mesh_shape)
+    # return the unstructured grid and the number of cells of the grid in each direction
+    return grid.cast_to_unstructured_grid(), mesh_shape
+
+
+def find_cells_within_bounds(mesh, bounds):
+    """
+    Find the index of cells in this mesh within bounds.
+    :param mesh: pyvista mesh
+    :param bounds: type:　iterable(float)
+        list of 6 values, [xmin, xmax, ymin, ymax, zmin, zmax]
+    :return: type: numpy.ndarray
+        array of cell indices
+    
+    example:
+        >> mesh = pv.PolyData(np.random.rand(10, 3))
+        >> indices = find_cells_within_bounds(mesh, [0, 1, 0, 1, 0, 1])
+    """
+    from pyvista import _vtk
+    from pyvista import vtk_id_list_to_array
+
+    if np.array(bounds).size != 6:
+        raise TypeError("Bounds must be a length three tuple of floats.")
+    locator = _vtk.vtkCellTreeLocator()
+    locator.SetDataSet(mesh)
+    locator.BuildLocator()
+    id_list = _vtk.vtkIdList()
+    locator.FindCellsWithinBounds(list(bounds), id_list)
+    return vtk_id_list_to_array(id_list)
+
+
+def label_mask(mesh_background, mesh_tri, tolerance=0.0000001):
+    """
+    Store the label of each fiber tow for intersection detection.
+    :param mesh_background: background mesh, type: pyvista.UnstructuredGrid
+    :param mesh_tri: tubular mesh of the fiber tows
+    :param tolerance: tolerance for the enclosed point detection
+    :return: mask: type: numpy.ndarray (bool)
+        mask of the background mesh, True for the cells that are within the bounds of the tubular mesh
+    :return: label_yarn: type: numpy.ndarray (int) (1D)
+    """
+    # extract the cell centers of the background mesh
+    cellCenters = mesh_background.cell_centers().points
+    points_poly = pv.PolyData(cellCenters)
+    # find the cells that are within the tubular surface of the fiber tow
+    select = points_poly.select_enclosed_points(mesh_tri, tolerance=0.0000001, check_surface=True)
+    mask = select['SelectedPoints'] == 1
+    label_yarn = select['SelectedPoints']
+    return mask, label_yarn
+
+
+def save_nrrd(cell_label, mesh_shape, file_path_name):
+    """
+    Save a mesh to a nrrd file.
+    :param cell_label: the cell label of the mesh, type: 1d numpy.ndarray
+    :param mesh_shape: number of elements in x, y, and z direction, type: numpy array(int, int, int)
+    :param file_path_name: the path and name of the nrrd file, without extension, type: str
+    :return: None
+    """
+    import nrrd
+
+    yarnIndex = cell_label
+
+    nx, ny, nz = mesh_shape
+
+    yarnIndex = yarnIndex.reshape((nz, ny, nx))
+
+    data = yarnIndex + 1
+    data = np.int32(data)
+
+    header = {'space origin': [0, 0, 0],
+              "space directions": [[1, 0, 0], [0, 1, 0], [0, 0, 1]],
+              'space': 'left-anterior-superior'}
+
+    # Write to a NRRD file with pynrrd
+    nrrd.write(file_path_name + ".nrrd", data, header)
+    return None
+
+
+def intersection_detect(label_set_dict):
+    """
+    Find the intersection of fiber tows from implicit surface.
+    :param label_set_dict: dictionary of label sets, type: dict
+        key: yarn indices, value: sparse matrix of cell queries
+    :return: type: dictionary of the indices of intersected cell
+        key: yarn indices 1_yarn indices 2, value: sparse matrix of cell indices
+    """
+    import itertools
+    from scipy.sparse import csr_matrix, coo_matrix
+
+    # length and keys of the dictionary
+    length = len(label_set_dict)
+    keys = label_set_dict.keys()
+    # possible intersection of cell indices
+    tows_combination_list = list(itertools.combinations(keys, 2))
+
+    print(tows_combination_list)
+
+    intersect_info = []
+    intersect_info_dict = {}
+    for i, key in enumerate(tows_combination_list):
+        # print the intersection checking progress
+        # print("Intersection check of Yarn %d and %d " % (key[0], key[1]))
+        # find the intersection of 2 yarns
+        intersection = label_set_dict[key[0]] + label_set_dict[key[1]]
+        # check if any element in the sparse matrix intersection is larger than 1
+        if np.any(intersection.data > 1):
+            print("Intersection found between Yarn %d and %d" % (key[0], key[1]))
+            n_cells_yarn1 = label_set_dict[key[0]].data.size
+            n_cells_yarn2 = label_set_dict[key[1]].data.size
+            n_cells_intersection = np.sum(intersection.data > 1)
+            # find the ratio of intersected cells to the total cells in each yarn
+            ratio_yarn1 = n_cells_intersection / n_cells_yarn1 * 100
+            ratio_yarn2 = n_cells_intersection / n_cells_yarn2 * 100
+            # print the ratio of intersected cells to the total cells in each yarn
+            print("Ratio of intersected cells to the total cells in each yarn: \n"
+                  " %.2f%% ( %d / %d) and %.2f%% (%d / %d)" % (ratio_yarn1, n_cells_intersection,
+                                                               n_cells_yarn1, ratio_yarn2, n_cells_intersection,
+                                                               n_cells_yarn2))
+            # print a dash line to separate the information
+            print("-" * 50)
+            # [yarn1, yarn2, intersected cells, cells in yarn 1, ratio of intersected cells in yarn 1,
+            # cells in yarn2, ratio of intersected cells in yarn 2]
+            intersect_info.append([key[0], key[1], n_cells_intersection,
+                                   n_cells_yarn1, ratio_yarn1, n_cells_yarn2, ratio_yarn2])
+            # save the intersection information in a dictionary
+            intersect_info_dict[key] = intersection
+
+    # summarize all the labels in label_set_dict
+    keys = label_set_dict.keys()
+    for key in keys:
+        try:
+            cell_data_intersect += label_set_dict[key]
+        except NameError:
+            cell_data_intersect = label_set_dict[key]
+
+    return intersect_info, intersect_info_dict, np.array(cell_data_intersect.todense())[0]
+
+
+def structured_cylinder_vertices(a, b, h, theta_res=5, h_res=5):
+    """
+    Generate points on a ellipse
+    :param a: semi-major axis
+    :param b: semi-minor axis
+    :param h: height
+    :param theta_res: number of points
+    :param h_res: number of points
+    :return:  vertices on the ellipse
+    """
+    theta_resolution = theta_res + 1
+    theta = np.linspace(0, 2 * np.pi, theta_resolution)
+    x = a * np.cos(theta)
+    y = b * np.sin(theta)
+    z = np.linspace(0, h, h_res, endpoint=True)
+
+    points = np.zeros((theta_resolution * h_res, 3))
+    for i in np.arange(h_res):
+        points[theta_resolution * i:theta_resolution * (i + 1), :2] = np.vstack((x, y)).T
+        points[theta_resolution * i:theta_resolution * (i + 1), 2] = z[i]
+
+    return points
+
+
+def tubular_mesh_generator(theta_res, h_res, vertices, plot=True):
+    """
+    Generate a tubular mesh
+    :param theta_res: number of points
+    :param h_res: number of points
+    :param vertices: vertices of the tubular mesh, shape (n, 3)
+        The vertices of the tubular mesh are sorted in the radial direction first,
+        then in the vertical direction. The first vertex is repeated
+        at the end of each radial direction point list.
+    :return:  points on the tubular mesh
+    """
+    import pyvista as pv
+    mesh = pv.CylinderStructured(theta_resolution=theta_res + 1,
+                                 z_resolution=h_res)
+    mesh.points = vertices
+    if plot:
+        mesh.plot(show_edges=True)
+    return mesh
+
+
+def to_meshio_data(mesh, theta_res, correction=True):
+    """
+    Convert PyVista flavor data structure to meshio.
+    :param mesh: PyVista.DataSet - Any PyVista mesh/spatial data type.
+
+    """
+    try:
+        import meshio
+    except ImportError:  # pragma: no cover
+        raise ImportError("To use this feature install meshio with:\n\npip install meshio")
+
+    try:  # for meshio<5.0 compatibility
+        from meshio.vtk._vtk import vtk_to_meshio_type
+    except:  # noqa: E722 pragma: no cover
+        from meshio._vtk_common import vtk_to_meshio_type
+
+    # Cast to pyvista.UnstructuredGrid
+    if not isinstance(mesh, pv.UnstructuredGrid):
+        mesh = mesh.cast_to_unstructured_grid()
+
+    # Copy useful arrays to avoid repeated calls to properties
+    vtk_offset = mesh.offset
+    vtk_cells = mesh.cells
+    vtk_cell_type = mesh.celltypes
+
+    # Check that meshio supports all cell types in input mesh
+    pixel_voxel = {8, 11}  # Handle pixels and voxels
+    for cell_type in np.unique(vtk_cell_type):
+        if cell_type not in vtk_to_meshio_type.keys() and cell_type not in pixel_voxel:
+            raise TypeError(f"meshio does not support VTK type {cell_type}.")
+
+    # Get cells
+    cells = []
+    c = 0
+    for offset, cell_type in zip(vtk_offset, vtk_cell_type):
+        numnodes = vtk_cells[offset + c]
+        if _vtk.VTK9:  # must offset by cell count
+            cell = vtk_cells[offset + 1 + c: offset + 1 + c + numnodes]
+            c += 1
+        else:
+            cell = vtk_cells[offset + 1: offset + 1 + numnodes]
+        cell = (
+            cell
+            if cell_type not in pixel_voxel
+            else cell[[0, 1, 3, 2]]
+            if cell_type == 8
+            else cell[[0, 1, 3, 2, 4, 5, 7, 6]]
+        )
+        cell_type = cell_type if cell_type not in pixel_voxel else cell_type + 1
+        cell_type = vtk_to_meshio_type[cell_type] if cell_type != 7 else f"polygon{numnodes}"
+
+        if len(cells) > 0 and cells[-1][0] == cell_type:
+            cells[-1][1].append(cell)
+        else:
+            cells.append((cell_type, [cell]))
+
+    for k, c in enumerate(cells):
+        cells[k] = (c[0], np.array(c[1]))
+
+    # Get point data
+    point_data = {k.replace(" ", "_"): v for k, v in mesh.point_data.items()}
+
+    # Get cell data
+    vtk_cell_data = mesh.cell_data
+    n_cells = np.cumsum([len(c[1]) for c in cells[:-1]])
+    cell_data = (
+        {k.replace(" ", "_"): np.split(v, n_cells) for k, v in vtk_cell_data.items()}
+        if vtk_cell_data
+        else {}
+    )
+
+    points = np.array(mesh.points)
+    cells = cells
+    if correction:
+        points, cells = mesh_correction(cells, points, theta_res=theta_res)
+
+    point_data = np.array(point_data)
+    cell_data = cell_data
+
+    return points, cells, point_data, cell_data
+
+
+def mesh_correction(cells, points, theta_res):
+    cells_connectivity = cells[0][1]
+
+    # 遍历cells中的每一个元素，如果元素中的点在rm_row_ind中，则将该元素减小theta_res
+    rm_row_ind = np.arange(theta_res, points.shape[0] + 2, theta_res + 1)
+    for i in range(cells_connectivity.shape[0]):
+        for j in range(cells_connectivity.shape[1]):
+            if cells_connectivity[i][j] in rm_row_ind:
+                # print(cells_connectivity[i][j])
+                cells_connectivity[i][j] -= theta_res
+
+    # get index of boundary points
+    first_boundary_ind = np.arange(theta_res)
+    second_boundary_ind = np.arange(points.shape[0] - 1 - theta_res, points.shape[0] - 1)
+    # coordinates of boundary points
+    first_boundary = points[first_boundary_ind]
+    second_boundary = points[second_boundary_ind]
+    # calculate the centroids of the boundaries
+    centroid_first_boundary = np.mean(first_boundary, axis=0)
+    centroid_second_boundary = np.mean(second_boundary, axis=0)
+    # add the centroids to the points
+    points = np.vstack((points, centroid_first_boundary, centroid_second_boundary))
+
+    # new cells
+    first_boundary_new_cell = [np.array([points.shape[0] - 2, i, i + 1]) for i in first_boundary_ind[:-1]]
+    first_boundary_new_cell.append(np.array([points.shape[0] - 2, first_boundary_ind[0], first_boundary_ind[-1]]))
+    second_boundary_new_cell = [np.array([points.shape[0] - 1, i, i + 1]) for i in second_boundary_ind[:-1]]
+    second_boundary_new_cell.append(np.array([points.shape[0] - 1, second_boundary_ind[0], second_boundary_ind[-1]]))
+    new_cells = first_boundary_new_cell + second_boundary_new_cell
+
+    cells = [('quad', list(cells_connectivity)), ('triangle', new_cells)]
+
+    return points, cells
+
+
+def unit_vector(vector):
+    vector = np.array(vector)
+    """ Returns the unit vector of the vector.  """
+    return vector / np.linalg.norm(vector)
+
+
+def angle_between(v1, v2):
+    """ Returns the angle in radians between vectors 'v1' and 'v2'::
+    numpy.clip(a, a_min, a_max, out=None, **kwargs)[source]
+    Clip (limit) the values in an array.     Given an interval, values outside the interval
+    are clipped to the interval edges. For example, if an interval of [0, 1] is specified,
+    values smaller than 0 become 0, and values larger than 1 become 1.
+    Equivalent to but faster than np.minimum(a_max, np.maximum(a, a_min)).
+    No check is performed to ensure a_min < a_max.
+    """
+    v1_u = unit_vector(v1)
+    v2_u = unit_vector(v2)
+    return np.rad2deg(np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0)))
+
+
+if "__name__" == "__main__":
+    bbox = np.array((0.0, 12.21, 0.5, 10.4, 0.20, 5.37))
+    voxel_size = [0.11, 0.11, 0.044]
+
+    mesh_background = background_mesh_generator(bbox, voxel_size)
+
+    # mesh_background.save('./test_bbox_voxel.vtu', binary=True)
---
+++ build/lib/polykriging/paraSurface.py
@@ -0,0 +1,201 @@
+# ！/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import sympy as sym
+
+
+#  Drift and Covariance
+def func_select(drift_name, cov_name):
+    '''
+    This is the function for definition of drift function and covariance function
+    in dictionary drif_funcs and cov_funcs.
+    '''
+    # Definitions of drift functions by dictionary
+    import sympy as sym
+
+    drift_funcs = {
+        'const': lambda x, a: [a[0]],
+        'lin': lambda x, a: [a[0], a[1] * x],
+        'quad': lambda x, a: [a[0], a[1] * x, a[2] * (x ** 2.0)]
+    }
+    # Definitions of covariance functions by dictionary
+    cov_funcs = {
+        'lin': lambda h: h,
+        'cub': lambda h: h ** 3.0,
+        # Natural logarithm, element-wise. Does not work in this version!!!
+        'log': lambda h: h ** 2.0 * sym.log(h) if h != 0 else 0,
+    }
+    # Int number of 'a_len', which is based on the drift function, will be used in building of kriging matrix.
+    a_len = len(drift_funcs[drift_name](0, [1, 1, 1, 1, 1, 1]))
+    return drift_funcs[drift_name], cov_funcs[cov_name], a_len
+
+
+def dist1D(x):
+    """
+    Calculate the distance between each pair of points.
+    :param x: numpy array. The coordinates in parametric space of the points. The shape is (m, 1).
+    :return: numpy array. The distance between each pair of points. The shape is (m, m).
+    """
+    b_len = x.size
+
+    xc = np.repeat(x, repeats=b_len, axis=0)
+    xc = xc.reshape((b_len, b_len))
+    xr = xc.T
+
+    return np.abs(xc - xr)
+
+
+def kVector(x, symVar, drift_name, cov_name):
+    """
+    Calculate the kriging matrix.
+    :param x: numpy array. The coordinates in parametric space of the points. The shape is (m, 1).
+    :param symVar: String. The variable in parametric space.
+    :param cov_name: String. The name of covariance function.
+    :return: numpy array. The kriging matrix. The shape is (m, 1).
+    """
+    s = sym.Symbol(symVar)
+    drift_func, cov_func, a_len = func_select(drift_name, cov_name)
+    # TODO: drift term
+    kVec = cov_func(np.abs(x - s))
+
+    adef = [1, 1, 1, 1, 1, 1, 1, 1]
+    a = drift_func(s, adef)
+    # append elements of drift function to kVector
+    kVec = np.append(kVec, a)
+    return kVec
+
+
+def buildM(x, drift_name, cov_name):
+    """
+    Build the kriging matrix.
+    :param x: The coordinates of the points. The shape is (m, 2).
+    :param drift_name: str. The name of the drift function.
+        Possible values are: "const", "lin", "quad".
+    :param cov_name: str. The name of the covariance function.
+        Possible values are: "lin", "cub", "log".
+    :return drift_func: The drift function.
+    :return cov_func: The covariance function.
+    :return a_len: The length of the drift function.
+    :return M: The matrix of the kriging system. The shape is (n,n).
+    """
+    # ------------drift and covariance function selection------------
+    drift_func, cov_func, a_len = func_select(drift_name, cov_name)
+
+    # -------- distance between each pair of points --------
+    distance = dist1D(x)
+
+    # ------------initialize the kriging matrix------------
+    b_len = x.shape[0]
+    n = b_len + a_len
+    M = np.zeros((n, n))
+
+    # -------- assembling the kriging matrix --------
+    M[:b_len, :b_len] = cov_func(distance)
+
+    # -------- elements depending on drift function --------
+    adef = [1, 1, 1, 1, 1, 1, 1, 1]
+    a = drift_func(x, adef)
+
+    for i in np.arange(a_len):
+        M[:b_len, b_len + i] = a[i]
+        M[b_len + i, :b_len] = a[i]
+    return M, drift_func, cov_func, a_len
+
+
+def nugget(M, nugg, b_len):
+    """
+    Introduce the nugget effect to the kriging matrix.
+    :param M:   The kriging matrix.
+    :param nugg:    The nugget effect.
+    :return:    The kriging matrix with nugget effect.
+    """
+    # -------- identity matrix with the same size as M --------
+    I = np.identity(b_len)
+    # -------- multiply nugg to the diagonal of I --------
+    I = I * nugg
+    # -------- add I to M --------
+    M[:b_len, :b_len] = M[:b_len, :b_len] + I
+    return M
+
+
+def buildP(x, a_lenS, a_lenT):
+    """
+    Build the result vector of the kriging linear system.
+    :param z: The values of the target function. The shape is (m,n).
+    :param a_len: The length of the drift function.
+    :return: The result vector of the kriging linear system. The shape is (n,).
+    """
+    P = np.append(x, np.zeros((a_lenS, x.shape[1])), axis=0)
+    P = np.append(P, np.zeros((P.shape[0], a_lenT)), axis=1)
+    return P
+
+
+def buildKriging(s, t, x, drift_names, cov_names, nugg=[0, 0]):
+    """
+    Build the kriging model and return the expression in string format.
+    :param s, t: numpy array. The parameters of the two profiles for surface parametric kriging.
+    :param x: array like. The known values of the variables in parametric space.
+    :param drift_names: list. The name of the drift functions for profile 1 and profile 2 in the
+        following format: [drift_name1, drift_name2]. The possible values are: 'const', 'lin', 'cub'.
+    :param cov_names: list. The name of the covariance functions in the following format:
+        [covariance_name1, covariance_name2]. The possible values are: 'lin', 'cub', 'log'.
+    :param nugg: list. The nugget effects (variance) for each profile contained in a list.
+    :return expr: The expression of kriging function in string format.
+    """
+    s, t, x = np.array(s), np.array(t), np.array(x)
+
+    # ------- build the kriging matrix -------
+    MS, drift_funcS, cov_funcS, a_lenS = buildM(s, drift_names[0], cov_names[0])
+    MT, drift_funcT, cov_funcT, a_lenT = buildM(t, drift_names[1], cov_names[1])
+
+    # ------- introduce nugget effect -------
+    MS = nugget(MS, nugg[0], a_lenS)
+    MT = nugget(MT, nugg[1], a_lenT)
+
+    # ------- build parametric kriging vectors -------
+    k1s = kVector(s, 's', drift_names[0], cov_names[0])
+    k2t = kVector(t, 't', drift_names[1], cov_names[1])
+
+    # ------- build the matrix of known variables P -------
+    Px = buildP(x, a_lenS, a_lenT)
+
+    # ------- build the parametric kriging model -------
+    expr = np.linalg.multi_dot([k1s.reshape([1, -1]), np.linalg.inv(MS),
+                                Px, np.linalg.inv(MT), k2t.reshape([-1, 1])]).sum()
+    return expr
+
+
+def interp(s, t, expr):
+    """
+    Interpolation (substitute the symbolic variables in the expression).
+    :param s, t: numpy array. The parameters of the two profiles for surface parametric kriging.
+    :param expr: String. The expression of the target function.
+    :return: The values of the kriging function. The shape is (s.size,t.size).
+    """
+    sVar, tVar = sym.symbols('s t')
+    xinterp = np.zeros((len(s), len(t)))
+    for i in range(len(s)):
+        for j in range(len(t)):
+            xinterp[i, j] = sym.simplify(expr.subs({sVar: s[i], tVar: t[j]}))
+    return xinterp
+
+
+if __name__ == '__main__':
+    x = [[1, 1], [np.sqrt(2) / 2, 1 / 2], [0, 0]]
+    y = [[0, 0], [np.sqrt(2) / 2, 1 / 2], [1, 1]]
+    z = [[0, 1], [0, 1], [0, 1]]
+
+    # define two symbolic variables (parameters) s and t
+    s = [0, 0.5, 1]
+    t = [0, 1]
+
+    nugg = [0, 0]
+
+    xexpr = buildKriging(s, t, x, ['lin', 'const'], ['cub', 'lin'], nugg)
+    yexpr = buildKriging(s, t, y, ['lin', 'const'], ['cub', 'lin'], nugg)
+    zexpr = buildKriging(s, t, z, ['lin', 'const'], ['cub', 'lin'], nugg)
+
+    xinterp = interp(s, t, xexpr)
+    yinterp = interp(s, t, yexpr)
+    zinterp = interp(s, t, zexpr)
