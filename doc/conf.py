0027fb1e7480697d8ccc235dfdb1edf8b77e83d5
Bin Yang <yangbin880101@163.com>
2024-03-03 20:44:49 -0500

    build for readthedocs

--- doc/conf.py
+++ doc/conf.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.abspath('./../polytex'))  # 指向src目录
 project = 'PolyTex'
 copyright = '2022-2024, Bin Yang'
 author = 'Bin Yang'
-release = '0.3.10'
+release = '0.3.12'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
